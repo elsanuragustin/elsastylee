718a533a14c0286621afc82d590f355c25797282
elsanuragustin <93563142+elsanuragustin@users.noreply.github.com>
2022-02-22 10:24:45 +0700

    update!!

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import "bootstrap/dist/css/bootstrap.min.css";
-import VueSupabase from "vue-supabase";
+import "bootstrap/dist/css/bootstrap.css";
+import VueSupabase from 'vue-supabase';
 
 
 Vue.use(VueSupabase, {
